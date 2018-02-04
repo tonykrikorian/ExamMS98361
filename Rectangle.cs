3a8576be3959780b10351d206074030baf15214c
Tony Krikorian <tony.krikorian@outlook.com>
2018-02-04 13:45:04 -0300

    Tercer commit

---
+++ Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamMS98361.Console
+{
+    class Rectangle
+    {
+        private double width;
+        private double length;
+
+        public Rectangle(double w, double l)
+        {
+            this.width = w;
+            this.length = l;
+        }
+
+        public double GetArea()
+        {
+            return width * length;
+        }
+    }
+}
